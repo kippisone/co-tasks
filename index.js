0fcf5631ccf4aeba5eaa3671dabf18c41613aa12
AndiOxidant <andi.oxidant@noname-media.com>
2015-12-26 11:10:12 +0100

    Add run method

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+'use strict';
+
+var co = require('co-utils');
+
 class CoTasks {
     constructor() {
+        /**
+         * Tasks storage
+         * @type {Object}
+         */
         this.tasks = {};
+
+        /**
+         * Predefined task names
+         * @type {Array}
+         * @default null
+         */
+        this.allowedTasks = null;
     }
 
     /**
@@ -10,8 +25,33 @@ class CoTasks {
      * @param {String|Array} [tasks] Task name to be run. If this is not set, all tasks will be run
      * @return {Object} Returns a promise
      */
-    run() {
+    run(tasks) {
+        if (!tasks) {
+            tasks = this.allowedTasks;
+        }
+        else if (typeof tasks === 'string') {
+            tasks = [tasks];
+        }
 
+        return co(function* () {
+            for (let task of tasks) {
+                if (!this.tasks[task]) {
+                    throw new Error('Task name ' + task + ' not defined!');
+                }
+
+                if (this.tasks['pre-' + task].length) {
+                    yield co.series(this.tasks['pre-' + task]);
+                }
+
+                if (this.tasks[task].length) {
+                    yield co.series(this.tasks[task]);
+                }
+
+                if (this.tasks['post-' + task].length) {
+                    yield co.series(this.tasks['post-' + task]);
+                }
+            }
+        }.bind(this));
     }
 
     /**
@@ -25,12 +65,14 @@ class CoTasks {
      */
     registerTask(name, fn) {
         if (!this.tasks[name]) {
-            if (this.checkTaskNames) {
+            if (this.allowedTasks) {
                 throw new Error('Task name ' + name + ' not defined!\nAllowed tasks are: ' + Object.keys(this.tasks).join(', '));
             }
 
-            this.tasks.name = [];
+            this.tasks[name] = [];
         }
+
+        this.tasks[name].push(fn);
     }
 
     /**
@@ -43,16 +85,19 @@ class CoTasks {
      * @return {Object} Returns this value
      */
     defineTasks(tasks, regPreTasks, regPostTasks) {
-        this.checkTaskNames = true;
+        this.allowedTasks = [];
         for (let task of tasks) {
             if (regPreTasks) {
-                this.tasks['pre-' . task] = [];
+                this.tasks['pre-' + task] = [];
+                this.allowedTasks.push('pre-' + task);
             }
 
             this.tasks[task] = [];
+            this.allowedTasks.push(task);
 
             if (regPostTasks) {
-                this.tasks['post-' . task] = [];
+                this.tasks['post-' + task] = [];
+                this.allowedTasks.push('post-' + task);
             }
         }
     }
--- tests/coTasks.spec.js
+++ tests/coTasks.spec.js
@@ -1,11 +1,182 @@
-var coTask = require('../index.js');
+'use strict';
+
+var CoTasks = require('../index.js');
+var expect = require('expect.js');
+var sinon = require('sinon');
 
 describe('co-tasks', function() {
-    'use strict';
-    
     describe('Constructor', function() {
-        it('Should be a CoTask class', function() {
-            expect(coTask).to.be.a('function');            
+        it('Should be a CoTasks class', function() {
+            expect(CoTasks).to.be.a('function');      
+        });
+
+        it('Should be an instance of CoTasks', function() {
+            var coTasks = new CoTasks();
+            expect(coTasks).to.be.a(CoTasks); 
+        });
+    });
+
+    describe('defineTasks', function() {
+        var taskRunner;
+
+        beforeEach(function() {
+            taskRunner = new CoTasks();
+        });
+
+        it('Should pre define task names', function() {
+            taskRunner.defineTasks(['foo', 'bar']);
+
+            expect(taskRunner.allowedTasks).to.eql(['foo', 'bar']);
+            expect(taskRunner.tasks).to.eql({
+                'foo': [],
+                'bar': []
+            });
+        });
+
+        it('Should pre define task names, including pre tasks', function() {
+            taskRunner.defineTasks(['foo', 'bar'], true);
+
+            expect(taskRunner.allowedTasks).to.eql(['pre-foo', 'foo', 'pre-bar', 'bar']);
+            expect(taskRunner.tasks).to.eql({
+                'pre-foo': [],
+                'foo': [],
+                'pre-bar': [],
+                'bar': []
+            });
+        });
+
+        it('Should pre define task names, including post tasks', function() {
+            taskRunner.defineTasks(['foo', 'bar'], false, true);
+
+            expect(taskRunner.allowedTasks).to.eql(['foo', 'post-foo', 'bar', 'post-bar']);
+            expect(taskRunner.tasks).to.eql({
+                'foo': [],
+                'post-foo': [],
+                'bar': [],
+                'post-bar': []
+            });
+        });
+
+        it('Should pre define task names, including pre and post tasks', function() {
+            taskRunner.defineTasks(['foo', 'bar'], true, true);
+
+            expect(taskRunner.allowedTasks).to.eql(['pre-foo', 'foo', 'post-foo', 'pre-bar', 'bar', 'post-bar']);
+            expect(taskRunner.tasks).to.eql({
+                'pre-foo': [],
+                'foo': [],
+                'post-foo': [],
+                'pre-bar': [],
+                'bar': [],
+                'post-bar': []
+            });
+        });
+    });
+
+    describe('registerTask', function() {
+        var taskRunner;
+
+        beforeEach(function() {
+            taskRunner = new CoTasks();    
+        });
+
+        it('Should register new tasks', function() {
+            var fn = function() {
+
+            };
+
+            taskRunner.registerTask('foo', fn);    
+            taskRunner.registerTask('foo', fn);    
+            taskRunner.registerTask('pre-foo', fn);    
+            taskRunner.registerTask('post-foo', fn);
+
+            expect(taskRunner.tasks).to.be.an('object');
+            expect(taskRunner.tasks).to.eql({
+                'foo': [fn, fn],
+                'pre-foo': [fn],
+                'post-foo': [fn]
+            });
+        });
+
+        it('Should register predefined tasks', function() {
+            var fn = function() {
+
+            };
+
+            taskRunner.defineTasks(['foo'], true, true);
+
+            taskRunner.registerTask('foo', fn);    
+            taskRunner.registerTask('foo', fn);    
+            taskRunner.registerTask('pre-foo', fn);    
+            taskRunner.registerTask('post-foo', fn);
+
+            expect(taskRunner.tasks).to.be.an('object');
+            expect(taskRunner.tasks).to.eql({
+                'foo': [fn, fn],
+                'pre-foo': [fn],
+                'post-foo': [fn]
+            });
+        });
+
+        it('Should fail by registering an unallowed task', function() {
+            var fn = function() {
+
+            };
+
+            taskRunner.defineTasks(['foo'], true, true);
+
+            taskRunner.registerTask('foo', fn);
+            expect(taskRunner.registerTask.bind(taskRunner)).withArgs('bar', fn).to.throwException((/not defined/));
+
+            expect(taskRunner.tasks).to.eql({
+                'pre-foo': [],
+                'foo': [fn],
+                'post-foo': []
+            });
+        });
+    });
+
+    describe('run', function() {
+        var taskRunner;
+
+        beforeEach(function() {
+            taskRunner = new CoTasks();    
+        });
+
+        it('Should run tasks', function(done) {
+            var fn1 = sinon.stub();
+            var fn2 = sinon.stub();
+            var fn3 = sinon.stub();
+            var fn4 = sinon.stub();
+
+            fn1.yields(null);
+            fn2.yields(null);
+            fn3.yields(null);
+            fn4.yields(null);
+
+            taskRunner.registerTask('foo', fn2);    
+            taskRunner.registerTask('foo', fn3);    
+            taskRunner.registerTask('pre-foo', fn1);    
+            taskRunner.registerTask('post-foo', fn4);
+
+            var promise = taskRunner.run('foo');
+            expect(promise).to.be.an(Promise);
+            expect(promise.then).to.be.an('function');
+            expect(promise.catch).to.be.an('function');
+
+            promise.then(function() {
+                expect(fn1.calledOnce).to.be.ok();
+                expect(fn2.calledOnce).to.be.ok();
+                expect(fn3.calledOnce).to.be.ok();
+                expect(fn4.calledOnce).to.be.ok();
+
+
+                expect(fn2.calledAfter(fn1)).to.be.ok();
+                expect(fn3.calledAfter(fn2)).to.be.ok();
+                expect(fn4.calledAfter(fn3)).to.be.ok();
+                done();
+            }).catch(function(err) {
+                done(err);
+            });
         });
     });
 });
