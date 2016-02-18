4fdb1230ad132ea9e0dad2a55aa64de929f7503a
Andifeind <andifeind@noname-media.com>
2016-02-18 23:09:46 +0100

    Changes

--- tests/tasks/tasktypes.js
+++ tests/tasks/tasktypes.js
@@ -18,4 +18,20 @@ module.exports = function(taskRunnner) {
     taskRunnner.registerTask('bla', function *() {
 
     });
+
+    taskRunnner.registerTask('foo', function(arg1, arg2, done) {
+        done();
+    });
+
+    taskRunnner.registerTask('bar', function(arg1, arg2, promise) {
+        return promise;
+    });
+
+    taskRunnner.registerTask('bar', Promise(function(resolve, reject) {
+
+    });
+
+    taskRunnner.registerTask('bla', function *(arg1, arg2) {
+
+    });
 };
